ed67aaf294d85406495540b9f40cb2da9cf015bf
Dickson <dicksonmelo@gmail.com>
2022-09-28 10:37:57 -0300

    test: setting up ex machina

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule DmBank.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:faker, "~> 0.17", only: :test}
+      {:faker, "~> 0.17", only: :test},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
