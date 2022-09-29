855ba263b869a1ae574974d94875be186b5a86b0
Dickson <dicksonmelo@gmail.com>
2022-09-29 16:40:04 -0300

    chore: adding new configs to user struct and adding registration changeset

--- lib/dm_bank/user_auth/user.ex
+++ lib/dm_bank/user_auth/user.ex
@@ -2,25 +2,20 @@ defmodule DmBank.UserAuth.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
     field :email, :string
     field :name, :string
-    field :password_hash, :string, default: "hash"
+    # the redact option hide the information in logs
+    field :password, :string, virtual: true, redact: true
+    field :password_hash, :string, redact: true
 
     timestamps()
   end
 
-  @spec changeset(
-          {map, map}
-          | %{
-              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
-              optional(atom) => any
-            },
-          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
-        ) :: Ecto.Changeset.t()
-  @doc false
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :email])
@@ -28,4 +23,25 @@ defmodule DmBank.UserAuth.User do
     # retornará o erro na hora do insert
     |> unique_constraint([:email])
   end
+
+  @doc false
+  def registration_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    # retornará o erro na hora do insert
+    |> unique_constraint([:email])
+    # compare with password_confirmation
+    |> validate_confirmation(:password)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    hash = Pbkdf2.hash_pwd_salt(password)
+
+    # adicionando a change no changeset
+    put_change(changeset, :password_hash, hash)
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
--- test/dm_bank/user_auth_test.exs
+++ test/dm_bank/user_auth_test.exs
@@ -10,26 +10,21 @@ defmodule DmBank.UserAuthTest do
 
     @invalid_attrs %{email: nil, name: nil, password_hash: nil}
 
-    test "list_users/0 returns all users" do
-      user = insert(:user)
-      assert UserAuth.list_users() == [user]
-    end
-
     test "get_user!/1 returns the user with given id" do
       user = insert(:user)
       assert UserAuth.get_user!(user.id) == user
     end
 
-    test "create_user/1 with valid data creates a user" do
+    test "register_user/1 with valid data creates a user" do
       valid_attrs = %{email: "john@email.com", name: "John Doe", password_hash: ""}
 
-      assert {:ok, %User{} = user} = UserAuth.create_user(valid_attrs)
+      assert {:ok, %User{} = user} = UserAuth.register_user(valid_attrs)
       assert user.email == "john@email.com"
       assert user.name == "John Doe"
     end
 
-    test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = UserAuth.create_user(@invalid_attrs)
+    test "register_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = UserAuth.register_user(@invalid_attrs)
     end
 
     test "update_user/2 with valid data updates the user" do
